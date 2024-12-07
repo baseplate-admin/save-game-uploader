573dbe35df2d552a6b568b7ea1498991cfd1cfc0
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-12-07 18:23:38 +0600

    Add more locations

--- src-tauri/src/save_games.rs
+++ src-tauri/src/save_games.rs
@@ -6,16 +6,16 @@ use std::{
     fs::File,
     io::BufReader,
     path::{Path, PathBuf},
-    thread,
-    time::Duration,
+    // thread,
+    // time::Duration,
 };
 use tauri::{AppHandle, Emitter};
 
 #[derive(Clone, Serialize)]
 struct EventPayload {
     name: String,
-    total: usize,
-    current: usize,
+    total: u64,
+    current: u64,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -34,22 +34,39 @@ pub async fn find_games(app_handle: AppHandle) -> Vec<LocationData> {
     let json_file = File::open(json_path).expect("`location.json` not found");
     let json_reader = BufReader::new(json_file);
 
-    let json: Vec<LocationData> = from_reader(json_reader).expect("JSON was not well-formatted");
+    let json: Vec<LocationData> =
+        from_reader(json_reader).expect("JSON was not well-formatted according to `LocationData`");
     let json_length = json.len();
 
     let mut found_games: Vec<LocationData> = Vec::new();
 
     'main: for (index, item) in json.into_iter().enumerate() {
         let parent_directory: PathBuf = match item.parent.as_str() {
+            // Public Directory
+            "Public_Document" => dirs_next::public_dir().unwrap().join("Documents"),
+            // User directories
             "Document" => dirs_next::document_dir().unwrap(),
+            "Local" => dirs_next::data_local_dir().unwrap(), // "AppData/Local"
+            "Roaming" => dirs_next::data_dir().unwrap(),     // "AppData/Roaming"
+            "ProgramData" => Path::new("C:\\ProgramData").to_path_buf(),
+
             _ => panic!("Parent Directory is invalid"),
         };
 
         let child_directory = parent_directory.join(item.directory.clone());
+        
+        println!(
+            "{} {} {}",
+            item.name,
+            child_directory.display(),
+            child_directory.exists()
+        );
+
         if !child_directory.exists() {
             continue 'main;
         };
 
+        // TODO: Make this function call threaded for increased performance?
         for glob_pattern in item.globs.clone() {
             let pattern_path = child_directory.join(glob_pattern.clone());
             let pattern_str = pattern_path.to_str().expect(&format!(
@@ -75,13 +92,15 @@ pub async fn find_games(app_handle: AppHandle) -> Vec<LocationData> {
                 "main-loop-progress",
                 EventPayload {
                     name: item.name.clone(),
-                    total: json_length,
-                    current: index + 1,
+                    total: json_length as u64,
+                    current: (index + 1) as u64,
                 },
             )
             .unwrap();
-        thread::sleep(Duration::from_millis(4000));
-        found_games.push(item); // Cloning the item to get an owned value
+
+        // Debug code for testing lode
+        // thread::sleep(Duration::from_millis(4000));
+        found_games.push(item);
     }
 
     found_games
