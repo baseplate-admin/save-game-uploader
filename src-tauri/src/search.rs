5cd63d8e251e2ac5aa1854eb4f72e665806a3edb
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-12-12 14:32:31 +0600

    f

--- src-tauri/src/search.rs
+++ src-tauri/src/search.rs
@@ -1,7 +1,34 @@
-use std::path::PathBuf;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+    sync::{Arc, Mutex},
+    thread,
+};
 use windows::{Win32::Foundation::MAX_PATH, Win32::Storage::FileSystem::GetLogicalDriveStringsW};
 
 use crate::debug_println;
+
+fn build_folder_map(dir: &Path, shared_vector: Arc<Mutex<Vec<PathBuf>>>) {
+    // Check if the path is a directory
+    if let Ok(entries) = fs::read_dir(dir) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                let path = entry.path();
+
+                // Push the folder or file name to the shared vector
+                {
+                    let mut vec = shared_vector.lock().unwrap();
+                    vec.push(path.clone());
+                }
+
+                // Recurse into directories
+                if path.is_dir() {
+                    build_folder_map(&path, Arc::clone(&shared_vector));
+                }
+            }
+        }
+    }
+}
 
 pub fn build_directory_map() -> Result<Vec<PathBuf>, String> {
     let paths: Vec<PathBuf> = Vec::new();
@@ -19,13 +46,20 @@ pub fn build_directory_map() -> Result<Vec<PathBuf>, String> {
     // Convert the buffer to a Rust string and split by null terminators
     let drive_strings = String::from_utf16_lossy(&buffer[..len as usize]);
     let drives: Vec<&str> = drive_strings
+        .clone()
         .split('\0')
         .filter(|s| !s.is_empty())
         .collect();
 
-    // Print each drive
+    let shared_directories: Arc<Mutex<Vec<PathBuf>>> = Arc::new(Mutex::new(Vec::<PathBuf>::new()));
+
+    let shared_vector_clone = Arc::clone(&shared_directories);
     for drive in drives {
-        println!("{}", drive);
+        let root_dir = Path::new(drive);
+        let handle = thread::spawn(move || {
+            build_folder_map(root_dir, shared_vector_clone);
+        });
     }
+
     Ok(paths)
 }
